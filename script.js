2d980e513ac60d4bf76149a2a046d9c7eafa3455
Allyson Pina <allyson.viana@edu.ufes.br>
2024-11-16 19:15:55 -0300

    adicionando dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    // if (html.classList.contains('light')) {
+    //     html.classList.remove('light')
+    // } else {
+    //     html.classList.add('light')
+    // }
+    /*o que está em cima vira isso aqui em baixo*/
+
+    html.classList.toggle('light')
+
+}
